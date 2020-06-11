b7f709a3c7ebf100d7b25b4dbf27b4da7e65b386
karlamartinezmoctez <karlamartinezmoctez@flpoly.org>
2020-06-11 19:54:37 -0400

    changes on the Form

--- tem-builder/src/Form.js
+++ tem-builder/src/Form.js
@@ -1,16 +1,29 @@
 //2. Creating a the Form component
-//importing React and useState
+//7. importing React and useState
 import React,{useState} from "react";
 
-import React from "react"
-
 const Form = (props) => {
+    //9. creating a state
+    const [person, setPerson] = useState({
+        title: ''
+    })
+
+    //10. onChange handler to control inputs
+    const handleChanges = e =>{
+        setPerson({title: e.target.value})
+        console.log(person);
+    }
     return(
         //6. creating the form
         <form>
+            {/* 7. Adding Labels */}
+            <label htmlfor="title">Team Member Role</label>
+            {/* 8.creating input */}
+            {/* 10. Add the onChange to the input*/}
+            <input id="title" onChange = {handleChanges}/>
 
         </form>
     )
-}
+};
 
 export default Form;
